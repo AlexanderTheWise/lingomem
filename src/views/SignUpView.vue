8b4b699e232f8144e8ae4f91327cc4db9a2fa2c1
AlexanderTheWise <aguillenhernandez@hotmail.com>
2023-08-18 21:02:19 +0200

    Add SingUp view

---
+++ src/views/SignUpView.vue
@@ -0,0 +1,26 @@
+<template>
+  <CredentialsForm @submit="handleSignUp" :submit-error="error">
+    <template #form-name> Sign up</template>
+    <template #form-link> Form link</template>
+  </CredentialsForm>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import CredentialsForm from "../components/forms/CredentialsForm.vue";
+import { signUp } from "../vueutils/auth";
+import type { UserCredentials } from "@/types";
+
+const error = ref("");
+
+const handleSignUp = async (credentials: UserCredentials) => {
+  const signUpError = await signUp(credentials);
+
+  if (signUpError) {
+    error.value = signUpError;
+    return;
+  }
+
+  error.value = "";
+};
+</script>
