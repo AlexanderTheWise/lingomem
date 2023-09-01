476d2d4a23355b6c6f78492c935b1367ce03eb88
AlexanderTheWise <aguillenhernandez@hotmail.com>
2023-09-01 19:10:22 +0200

    Add DeckCard component

---
+++ src/components/DeckCard.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-card class="deck ma-0 pa-1 rounded-lg d-flex flex-column">
+    <v-btn
+      class="deck-delete"
+      icon="mdi-delete-outline"
+      variant="tonal"
+      color="red-darken-4"
+      @click="deleteDeck(deck.id)"
+    >
+    </v-btn>
+
+    <div class="d-flex justify-center">
+      <v-img
+        alt="img"
+        max-width="150"
+        height="150"
+        cover
+        :src="deck.imageUrl"
+        class="rounded-circle"
+      ></v-img>
+    </div>
+
+    <div class="deck-content px-2 flex-grow-1 mt-1">
+      <div class="d-flex justify-space-between align-center mt-2">
+        <v-card-title class="pa-0">{{ deck.name }}</v-card-title>
+        <div class="text-white font-italic bg-green-accent-3 rounded-pill px-2">
+          to review: 4
+        </div>
+      </div>
+
+      <v-card-text class="pa-0">{{ deck.description }}</v-card-text>
+    </div>
+  </v-card>
+</template>
+
+<script setup lang="ts">
+import { inject } from "vue";
+import type { DeckCard } from "@/types";
+
+defineProps<{ deck: DeckCard }>();
+const deleteDeck = inject<(deckId: number) => Promise<void>>("deleteDeck")!;
+</script>
+
+<styled scoped lang="scss">
+.deck {
+  position: relative;
+  transition: transform 0.1s ease;
+
+  &:hover {
+    transform: translateY(-1.5%);
+  }
+
+  &-content {
+    border-top: 1px solid;
+    border-bottom: 1px solid;
+  }
+
+  .deck-delete {
+    position: absolute;
+    right: 1.5%;
+  }
+}
+</styled>
--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,35 @@ export interface UserCredentials {
   email: string;
   password: string;
 }
+
+export interface SRItem {
+  reviews: number;
+  efactor: number;
+  interval: number;
+}
+
+export interface Evalutation {
+  score: 1 | 2 | 3 | 4 | 5;
+  lateness: number;
+}
+
+export interface Deck {
+  id: number;
+  user_id: string;
+  name: string;
+  description: string;
+  imageUrl: string;
+  flashcards: Flashcards;
+}
+
+export interface Flashcard extends SRItem {
+  id: string;
+  question: string;
+  answer: string;
+  imageUrl: string;
+}
+
+export type Flashcards = Flashcard[];
+export type Decks = Deck[];
+export type DeckCard = Omit<Deck, "user_id" | "flashcards">;
+export type DeckCards = DeckCard[];
