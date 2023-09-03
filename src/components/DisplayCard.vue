e4631c9730cc2094d525971d22001a3cd25691b9
AlexanderTheWise <aguillenhernandez@hotmail.com>
2023-09-03 21:07:33 +0200

    Add DisplayCard component

---
+++ src/components/DisplayCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <FlashCard :flashcard="flashcard" />
+</template>
+
+<script setup lang="ts">
+import { ref, provide } from "vue";
+import type { Flashcard } from "@/types";
+import FlashCard from "./FlashCard.vue";
+
+defineProps<{ flashcard: Flashcard }>();
+const isFlipped = ref(false);
+
+function flip() {
+  isFlipped.value = !isFlipped.value;
+}
+
+provide("flipped", { isFlipped, flip });
+</script>
