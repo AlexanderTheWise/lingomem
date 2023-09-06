783f66dc04a55a17262186e70501cb43f659089f
AlexanderTheWise <aguillenhernandez@hotmail.com>
2023-09-06 23:38:02 +0200

    Add Submit button component

---
+++ src/components/SubmitButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-btn
+    type="submit"
+    size="large"
+    block
+    rounded="xl"
+    variant="flat"
+    color="teal-darken-4"
+    :disabled="disabled"
+  >
+    {{ submitName }}
+  </v-btn>
+</template>
+
+<script setup lang="ts">
+import { inject } from "vue";
+
+const submitName = inject<string>("submitName");
+defineProps<{ disabled: boolean }>();
+</script>
