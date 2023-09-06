c17d119e7551d72d1e5eb1076e961ad7ce1427cd
AlexanderTheWise <aguillenhernandez@hotmail.com>
2023-09-06 23:43:29 +0200

    Add TextInput component

---
+++ src/components/forms/TextInput.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-text-field
+    :prepend-icon="icon"
+    v-model="value"
+    :label="label"
+    variant="outlined"
+    density="compact"
+    :type="type"
+    class="mb-2"
+    :error="!!errors.length"
+    :error-messages="errors"
+    @input="emit('updateValue', value)"
+  >
+  </v-text-field>
+</template>
+
+<script setup lang="ts">
+import type { TextInputProps } from "@/types";
+import { useField } from "vee-validate";
+
+const emit = defineEmits<{ (event: "updateValue", value: string): void }>();
+
+const props = defineProps<TextInputProps>();
+
+const { value, errors } = useField<string>(props.path, props.schema.required());
+</script>
